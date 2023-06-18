c8e40fe542b7b007a0d18645da0358be55cb73c0
Huong <zoengo2002@gmail.com>
2023-06-17 23:32:46 -0700

    changed model and open ai version

--- app.py
+++ app.py
@@ -1,41 +1,34 @@
 import os
-# from translate import Translator
+from translate import Translator
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        english = request.form["english"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(english),
-            temperature=0.6,
+        medical_text = request.form["medical_text"]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": generate_prompt(medical_text)}]
         )
-        return redirect(url_for("index", result=response.choices[0].text))
-
+        return redirect(url_for("index", result=response['choices'][0].message.content.lstrip('\n')))
+    
     result = request.args.get("result")
+    # translated_result = translate_en_to_es(result)
     return render_template("index.html", result=result)
 
 
-def generate_prompt(english):
-    return """Give me a 20 sentence summary of the declaration of independence.""".format(
-        english.capitalize()
+def generate_prompt(medical_text):
+    return """Rephrase this text: "{}" into layman's terms 
+    so that a patient who is old and has very limited medical 
+    terminology knowledge will understand and keep it to 10 sentences""".format(
+        medical_text.capitalize()
     )
 
 def translate_en_to_es(text):
     translator = Translator(from_lang='en', to_lang='es')
     translation = translator.translate(text)
     return translation
-
-"""
- Example usage 
-english_text = "Hello, how are you?"
-translated_text = translate_text(english_text)
-print(translated_text)
-
-"""""""""
