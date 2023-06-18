6a2518f8b32a54fbab86873b9271379bde6dd0c3
Alayna Nguyen <32422836+alaynamnguyen@users.noreply.github.com>
2023-06-18 02:38:24 -0700

    Added some functionality to new simplify more button

--- app.py
+++ app.py
@@ -23,20 +23,51 @@ def index():
             tts = gTTS(result2, lang='en')
             tts.save('static/output.mp3')
 
-            return redirect(url_for("index", result=response['choices'][0].message.content.lstrip('\n')))
+            return redirect(url_for("more", result=response['choices'][0].message.content.lstrip('\n')))
         elif "restart" in request.form:
             response = ""
-            return redirect(url_for("index", result=""))
+            return redirect(url_for("index", result=response))
     
     result = request.args.get("result")
     # translated_result = translate_en_to_es(result)
     return render_template("index.html", result=result)
 
+@app.route("/more", methods=("GET", "POST"))
+def more():
+    if request.method == "POST":
+        if "morelaymans" in request.form:
+            medical_text = request.form["medical_text"]
+            simplified_text = generate_simplified_text(medical_text)
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": simplified_text}]
+            )
+
+            result2 = response.choices[0].message.content
+        
+            # Generate speech using gTTS
+            tts = gTTS(result2, lang='en')
+            tts.save('static/output.mp3')
+
+            return redirect(url_for("more", result=response['choices'][0].message.content.lstrip('\n')))
+        elif "restart" in request.form:
+            response = ""
+            return redirect(url_for("index", result=response))
+
+    result = request.args.get("result")
+    return render_template("more.html", result=result)
 
 def generate_prompt(medical_text):
     return """Rephrase this text: "{}" into layman's terms 
     so that a patient who is old and has very limited medical 
-    terminology knowledge will understand and keep it to 10 sentences""".format(
+    terminology knowledge will understand and keep it to 10 sentences.
+    Also include any potential risks mentioned to the patient.""".format(
+        medical_text.capitalize()
+    )
+
+def generate_simplified_text(medical_text):
+    return """Rephrase this text: "{}" to be a little bit simpler and keep it to 10 sentences.
+    """.format(
         medical_text.capitalize()
     )
 
