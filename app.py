c7dfcaf2620c3948ce8be15f94b76bec64f25804
Alayna Nguyen <32422836+alaynamnguyen@users.noreply.github.com>
2023-06-17 22:27:10 -0700

    Refactored variables

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        english = request.form["english"]
         response = openai.Completion.create(
-            model="gpt-3.5-turbo",
-            prompt=generate_prompt(animal),
+            model="text-davinci-003",
+            prompt=generate_prompt(english),
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -22,16 +22,9 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def generate_prompt(english):
+    return """Give me a 20 sentence summary of the declaration of independence.""".format(
+        english.capitalize()
     )
 
 def translate_en_to_es(text):
