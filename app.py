9ef911323d2c6337cbfeb3dcecc86eb9d070ef2c
Alayna Nguyen <32422836+alaynamnguyen@users.noreply.github.com>
2023-06-17 21:19:23 -0700

    Added quickstart files to repo

---
+++ app.py
@@ -0,0 +1,48 @@
+import os
+from translate import Translator
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        animal = request.form["animal"]
+        response = openai.Completion.create(
+            model="gpt-4",
+            prompt=generate_prompt(animal),
+            temperature=0.6,
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+def generate_prompt(animal):
+    return """Suggest three names for an animal that is a superhero.
+
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Animal: Dog
+Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: {}
+Names:""".format(
+        animal.capitalize()
+    )
+
+def translate_en_to_es(text):
+    translator = Translator(from_lang='en', to_lang='es')
+    translation = translator.translate(text)
+    return translation
+
+"""
+ Example usage 
+english_text = "Hello, how are you?"
+translated_text = translate_text(english_text)
+print(translated_text)
+
+"""""""""
---
+++ translate_en.py
@@ -0,0 +1,9 @@
+
+
+"""
+ Example usage 
+english_text = "Hello, how are you?"
+translated_text = translate_text(english_text)
+print(translated_text)
+
+"""
